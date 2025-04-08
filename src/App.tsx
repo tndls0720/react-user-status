d52fc0eb7d873ff66d592c718fd8bae7edcfdd1a
tndls0720 <tndls154154@gmail.com>
2025-04-08 13:19:31 +0900

    import 연결 및 상태관리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
+import "./App.css";
+import { MyButton } from "./components/MyButton";
+import UserStatus from "./components/UserStatus";
+import { useState } from "react";
 
 function App() {
-
+  const [isUserOnline, setIsUserOnline] = useState<boolean>(true);
+  const toggleStatus = () => {
+    setIsUserOnline((prev) => !prev);
+  };
 
   return (
-
-
-  )
+    <div>
+      <MyButton onClick={toggleStatus} />
+      <UserStatus isOnline={isUserOnline} />
+    </div>
+  );
 }
 
-export default App
+export default App;
