259815de22ef17dd3863ca99f4dab8ab6e617247
tndls0720 <tndls154154@gmail.com>
2025-04-10 18:27:39 +0900

    메뉴 아이템 컴포넌트

--- src/components/MyButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface MyBottonProps {
-  onClick: () => void;
-}
-
-export const MyButton: React.FC<MyBottonProps> = ({ onClick }) => {
-  return <button onClick={onClick}>상태변경</button>;
-};
