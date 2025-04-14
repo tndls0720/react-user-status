a0dbc0361b5866fdb02cebbe65a193e2118f8b49
tndls0720 <tndls154154@gmail.com>
2025-04-15 01:14:05 +0900

    헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mainpage from "./page/MainPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Mainpage />} />
       </Routes>
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Cafe</h1>
+      <nav>
+        <a href="/">홈</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
